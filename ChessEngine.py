aa5d9dba6b8e7e9671c7bb4600c544bd70842390
Kris <kris1toja@gmail.com>
2025-03-03 22:51:07 +0100

    repaired check mate assesment logic

--- ChessEngine.py
+++ ChessEngine.py
@@ -9,7 +9,8 @@ class ChessBoard():
         self.board = np.uint64(0)
         self.color = Color.WHITE
         self.move_log = [] 
-        self.inCheck = False
+        self.check_mate = False
+        self.stale_mate = False
         self.pins = []
         self.check = []
         self.captured_piece = False
@@ -31,8 +32,6 @@ class ChessBoard():
             if self.pieces[self.color][piece_type] & move.piece_moved:
                 self.pieces[self.color][piece_type] &= ~(np.uint64(move.piece_moved))     
                 if move.is_pawn_promotion:
-                    print(move.moved_piece_type)
-                    print(move.moved_piece_color)
                     self.pieces[self.color][Piece.QUEEN] |= np.uint64(move.piece_captured)
                 else:
                     self.pieces[self.color][piece_type] |= np.uint64(move.piece_captured)
@@ -82,58 +81,30 @@ class ChessBoard():
             self.board = self.combined_color[Color.WHITE] | self.combined_color[Color.BLACK]
 
     def get_valid_moves(self):
-        # Making a copy to not modify the original state of the board
+        # Making a copy to make sure we do not modify the original state of the board
         original_pieces = np.copy(self.pieces)
         original_combined_color = np.copy(self.combined_color)
         original_board = self.board
         original_color = self.color
         original_move_log = list(self.move_log)
 
-        moves = []
-        self.in_check, self.pins, self.checks = self.check_for_pins_and_checks()
-        king_bitmap = self.pieces[self.color][Piece.KING]
-        king_row, king_col = self.get_coordinates(king_bitmap)
-
-        if self.in_check:
-            if len(self.checks) == 1:  
-                moves = self.get_all_possible_moves()
-                check = self.checks[0]
-                check_row = check[0]
-                check_col = check[1]
-                piece_checking = self.get_piece_at(check_row, check_col)
-                valid_squares = []
-                
-                # If the checking piece is a knight, we can only capture it or move the king
-                if piece_checking[1] == Piece.KNIGHT:
-                    valid_squares = [(check_row, check_col)]
-                else:
-                    # For other pieces, we can block the check or capture the checking piece
-                    for i in range(1, 8):
-                        valid_square = (king_row + check[2] * i, king_col + check[3] * i)
-                        valid_squares.append(valid_square)
-                        if valid_square[0] == check_row and valid_square[1] == check_col:
-                            break
-                
-                # Filter moves that don't address the check
-                for i in range(len(moves) - 1, -1, -1):
-                    if moves[i].piece_moved != king_bitmap:  # If not a king move
-                        if not (moves[i].end_row, moves[i].end_col) in valid_squares:
-                            moves.remove(moves[i])
-            else:  
-                self.get_king_moves(king_row, king_col, moves)
-        else:
-            moves = self.get_all_possible_moves()
-        
-        legal_moves = []
-        for move in moves:
-            self.make_move(move)
-            self.color = ~self.color 
-            in_check, _, _ = self.check_for_pins_and_checks()
+        moves = self.get_all_possible_moves()
+        for i in range(len(moves) - 1,-1, -1):
+            self.make_move(moves[i])
+            self.color = ~self.color
+            if self.in_check():
+                moves.remove(moves[i])
             self.color = ~self.color
             self.undo_move()
-            
-            if not in_check:
-                legal_moves.append(move)
+        
+        if len(moves) == 0: #checkmate or stalemate
+            if self.in_check():
+                self.check_mate = True
+            else:
+                self.stale_mate = True
+        else:
+            self.check_mate = False
+            self.stale_mate = False
         
         # restoring original board state
         self.pieces = original_pieces
@@ -142,81 +113,23 @@ class ChessBoard():
         self.color = original_color
         self.move_log = original_move_log
         
-        return legal_moves
-
-    def is_checkmate(self):
-        if not self.in_check:
-            return False
-        
-        return len(self.get_valid_moves()) == 0
-
-    def is_stalemate(self):
-        if self.in_check:
-            return False
-        
-        return len(self.get_valid_moves()) == 0
-
-    def is_game_over(self):
-        if self.is_checkmate():
-            return True, "0-1" if self.color == Color.WHITE else "1-0"
-        elif self.is_stalemate():
-            return True, "1/2-1/2"
-        else:
-            return False, None
-
-    def check_for_pins_and_checks(self):
-        directions = ((-1, 0), (0, -1), (1, 0), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1))
-        pins = []
-        checks = []
-        in_check = False
+        return moves
+                        
+    def in_check(self):
         king_bitmap = self.pieces[self.color][Piece.KING]
-        start_row, start_col = self.get_coordinates(king_bitmap)
+        king_row, king_col = self.get_coordinates(king_bitmap)
+        return self.square_under_attack(king_row, king_col)
 
-        for i in range(len(directions)):
-            d = directions[i]
-            possible_pin = ()
-            for j in range(1, 8):
-                end_row = start_row + d[0] * j
-                end_col = start_col + d[1] * j
-                if 0 <= end_row <= 7 and 0 <= end_col <= 7:
-                    end_piece = self.get_piece_at(end_row, end_col)
-                    if end_piece is None:
-                        continue
-                    if end_piece[0] == self.color and end_piece[1] != Piece.KING:
-                        if possible_pin == ():
-                            possible_pin = (end_row, end_col, d[0], d[1])
-                        else:
-                            break
-                    elif end_piece[0] == ~self.color:
-                        type = end_piece[1]
-                        if(0 <= i <= 3 and type == Piece.BISHOP) or \
-                                (4 <= i <= 7 and type == Piece.ROOK) or \
-                                (j == 1 and type == Piece.PAWN and ((~self.color == Color.WHITE and 6 <= i <= 7) or (~self.color == Color.BLACK and 4 <= i <= 5))) or \
-                                (type == Piece.QUEEN) or (j == 1 and type == Piece.KING):
-                            if possible_pin == ():
-                                in_check = True
-                                checks.append((end_row, end_col, d[0], d[1]))
-                                break
-                            else:
-                                pins.append(possible_pin)
-                                break
-                        else:
-                            break
-                else:
-                    break
-        
-        knight_moves = [(1, -2), (1, 2), (-1, -2), (-1, 2), (-2, -1), (-2, 1), (2, -1), (2, 1)]
-        for m in knight_moves:
-            end_row = start_row + m[0]
-            end_col = start_col + m[1]
-            if 0 <= end_row <= 7 and 0 <= end_col <= 7:
-                end_piece = self.get_piece_at(end_row, end_col)
-                if end_piece is not None and end_piece[0] == ~self.color and end_piece[1] == Piece.KNIGHT:
-                    in_check = True
-                    checks.append((end_row, end_col, m[0], m[1]))
-        return in_check, pins, checks
-                        
-                        
+    def square_under_attack(self, r, c):
+        self.color = ~self.color
+        opp_moves = self.get_all_possible_moves()
+        self.color = ~self.color
+        for move in opp_moves:
+            if move.end_row == r and move.end_col == c:
+                return True
+        return False
+
+
     def get_all_possible_moves(self):
         moves = []
         current_pieces = self.combined_color[self.color]
